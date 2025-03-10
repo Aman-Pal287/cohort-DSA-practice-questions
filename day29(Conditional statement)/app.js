a6e66299d946c3c0f739bbaf1a1c818f457c43bd
Aman-Pal287 <palaman9786@gmail.com>
2025-03-10 15:28:15 +0530

    Leap year optimized method

--- day29(Conditional statement)/app.js
+++ day29(Conditional statement)/app.js
@@ -1,15 +1,31 @@
 // some if else questions and its importent 
-let year = Number(prompt("Enter a year"));
-let isLeap = false;
 
-if(year%4==0){
-    if(year%100==0){
-        if(year%400==0) isLeap = true;
-        else isLeap=false;
-    }
-    else{
-        isLeap = true;
-    }
-}else isLeap = false;
+// Q1 checking leap year
+//method:1
+// let year = Number(prompt("Enter a year"));
+// let isLeap = false;
 
-console.log(isLeap? "Leap Year":"Not a Leap Year")
+// if(year%4==0){
+//     if(year%100==0){
+//         if(year%400==0) isLeap = true;
+//         else isLeap=false;
+//     }
+//     else{
+//         isLeap = true;
+//     }
+// }else isLeap = false;
+
+// console.log(isLeap? "Leap Year":"Not a Leap Year")
+
+//method(efficient(optimized) method):2
+// let year = Number(prompt("Enter a year"));
+// let isLeap = false;
+// if((year%4==0 && year%100!=0)){
+//     console.log("Leap Year");
+// }else if (year%400==0){
+//     console.log("Leap Year")
+// }else console.log("Not a Leap year");
+
+
+// Q2. shop discount
+
