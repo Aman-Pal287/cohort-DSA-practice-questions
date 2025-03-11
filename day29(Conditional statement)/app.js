5fdc918be545046fb3ec4cf1aee053e0e3be92ef
Aman-Pal287 <palaman9786@gmail.com>
2025-03-11 15:09:26 +0530

    bijli bill brut force and efficient method se done

--- day29(Conditional statement)/app.js
+++ day29(Conditional statement)/app.js
@@ -50,3 +50,41 @@
 // else if(amount>9000) disount = 20;
 // else console.log("invalid input");
 // console.log(amount -  (disount*amount)/100);
+
+
+
+
+//Bijli ka bill question *very importent question in term of concepts
+
+//brut force method bolte hai isko (aur ye ghatiya tarika hai matlab efficient tarika nhi hai)
+// let unit = Number(prompt("Enter your month unit"));
+// let amount = 0;
+
+// if(unit>0 && unit<=100){
+//     amount = unit*4.2;
+// }else if(unit>100 && unit <=200){
+//     amount = (100*4.2) + (unit-100)*6;
+// }else if(unit>200 && unit<=400){
+//     amount = (100*4.2) + (100*6) + (unit-200)*8;
+// }else if(unit>400){
+//     amount = (100*4.2) + (100*6) + (200*8) + (unit-400)*13;
+// }
+// console.log("your bill amount = " + amount);
+
+//now efficient tarika dekho (pehle brut forc method mein hum upar se niche aaye the ab efficient tarika mein hum niche se upar jayenge)
+// let unit = Number(prompt("Enter your month unit"));
+// let amount = 0;
+// if(unit>400){
+//     amount = (unit-400)*13;
+//     unit = 400;
+// }
+// if(unit>200 && unit<=400){
+//     amount = amount + (unit-200)*8;
+//     unit = 200;
+// }
+// if(unit>100 && unit <=200){
+//     amount = amount + (unit-100)*6;
+//     unit = 100;
+// }
+// amount = amount + unit*4.2;
+// console.log("your bill amount = " + amount);
