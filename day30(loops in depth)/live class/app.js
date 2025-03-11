6c58c7b4a6617ba6b6e02fc22d1abee7b458d9f4
Aman-Pal287 <palaman9786@gmail.com>
2025-03-11 18:51:22 +0530

    problem 1 is done

---
+++ day30(loops in depth)/live class/app.js
@@ -0,0 +1,11 @@
+// problem 1: print hello world 5 times
+// let n = Number(prompt("Enter a number"));//5
+// if(isNaN(n)){
+//     console.log("Invalid Input");
+// }else{
+//     let i;
+//     for(i=1;i<=n;i++){
+//     console.log("hello world")  
+//     }
+//     console.log("failed at " + i );
+// }
