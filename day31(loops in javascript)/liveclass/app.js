2eb337375825f5f8e4c403ff42b7a720b1d249e5
Aman-Pal287 <palaman9786@gmail.com>
2025-03-12 12:36:00 +0530

    example 1 and 2 is done

---
+++ day31(loops in javascript)/liveclass/app.js
@@ -0,0 +1,39 @@
+//break statement
+// let n = 10;
+// for(let i = 1;i<=n;i++){
+//     if(i==8) break;
+//     console.log(i);
+// }
+// console.log("============================")
+// for(let i = 1;i<=n;i++){
+//     if(i==8) continue;
+//     console.log(i);
+// }
+
+
+
+//while loop
+
+//------------
+// declare.initialize 
+//while(//condition){
+
+    //change
+
+//}
+//---------------
+//example :1
+// let n = 5;
+// let i = 1;
+// while(i<=n){
+//     console.log("hello world");
+//     i++;
+// }
+
+//example:2
+// let n=5 , sum =0 , i=1;
+// while(i<=n){
+//     sum =sum+i
+//     i++;
+// }
+// console.log(sum);
