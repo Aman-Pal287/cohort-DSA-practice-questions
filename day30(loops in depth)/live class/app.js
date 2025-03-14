98729c54b87a2b17fd5706a261b0602fed3fad5e
Aman-Pal287 <palaman9786@gmail.com>
2025-03-15 01:41:11 +0530

    problem 8 is done

--- day30(loops in depth)/live class/app.js
+++ day30(loops in depth)/live class/app.js
@@ -77,3 +77,27 @@
 // }
 // console.log("the factors is = " + n)  
 
+
+
+//problem 8(Important): check if the number is Prime or not.
+
+// let n = Number(prompt("Enter a number"));
+// let isPrime = true;
+
+// method 1:
+// for (let i = 2; i<n; i++){ // loop 2 se (n-1) tak chalega
+//     if(n%i==0){
+//         isPrime = false;
+//         break
+//     }
+// }
+// console.log(isPrime ? "prime number" : "Not a prime number");
+
+// method 2 : (efficient tarika)
+// for (let i = 2; i<=n/2; i++){ // loop 2 se (n-1) tak chalega
+//     if(n%i==0){
+//         isPrime = false;
+//         break
+//     }
+// }
+// console.log(isPrime ? "prime number" : "Not a prime number");
