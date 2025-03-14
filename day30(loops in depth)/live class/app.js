668d093e7502cd5efa920452d07acff9f95c98b1
Aman-Pal287 <palaman9786@gmail.com>
2025-03-14 23:42:11 +0530

    problem 3 is done

--- day30(loops in depth)/live class/app.js
+++ day30(loops in depth)/live class/app.js
@@ -27,6 +27,7 @@
 
 
 //problem 3: take a number as input and print its table
-let 
+// let n = Number(prompt("Enter a number"));
+// for (let i = 1; i<=10;i++) console.log(n + " * " + i + " = " + (n*i));
 
 
