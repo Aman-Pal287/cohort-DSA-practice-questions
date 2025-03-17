1eb9653354ac197488ad3d74aa13becb749d968d
Aman-Pal287 <palaman9786@gmail.com>
2025-03-17 14:49:48 +0530

    prolem 1 is done

--- day32/live class/app.js
+++ day32/live class/app.js
@@ -1,8 +1,16 @@
 //do-while loop : exit control loop
 //example- for start of do-while loop
-let i = 1
-do{
-    console.log("namaste duniya!");
-    i++;
-}while(i<=5)
-    
+// let i = 1
+// do{
+//     console.log("namaste duniya!");
+//     i++;
+// }while(i<=5)
+
+
+
+// problem 1: repeat hello
+// let userInput;
+// do{
+//     console.log("namaste duniya");
+//     userInput = prompt("aap continue karna chahte ho kya? yes/no").toLowerCase();
+// }while(userInput === 'yes');
