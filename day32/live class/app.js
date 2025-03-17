702b887017e688d4ab49cefdf2b2151738842483
Aman-Pal287 <palaman9786@gmail.com>
2025-03-17 15:23:43 +0530

    prolem 2 is done

--- day32/live class/app.js
+++ day32/live class/app.js
@@ -14,3 +14,18 @@
 //     console.log("namaste duniya");
 //     userInput = prompt("aap continue karna chahte ho kya? yes/no").toLowerCase();
 // }while(userInput === 'yes');
+
+
+//problem 2 : guess the number ek program bnao
+// let com = Math.floor(Math.random() * 100) + 1;
+// let userInput;
+// do {
+//     userInput = Number(prompt("Guess the number in b/w 1 to 100"))
+//     if (isNaN(userInput) || userInput < 0 || userInput > 100) {
+//         console.log("please enter a valid number");
+//         continue
+//     }
+//     if (userInput > com) console.log("too high, try again");
+//     else if (userInput < com) console.log("too low, try again");
+//     else console.log("congrats 🎉 and number was " + com);
+// } while (userInput !==   com); 
