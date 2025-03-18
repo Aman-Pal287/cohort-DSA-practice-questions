1b1692526165093836e7f330d90e33475aacd703
Aman-Pal287 <palaman9786@gmail.com>
2025-03-18 13:37:59 +0530

    question1 is done

---
+++ day32/assignment/app.js
@@ -0,0 +1,15 @@
+// Question 1: checking isbn number
+// let n = Number(prompt("Enter a isbn number"));
+// let copy = n;
+// let i= 10 , sum = 0 ,isbnNum = 0 , rem = 0;
+// do{
+//     rem = n%10;
+//     sum = rem * i;
+//     i--;
+//     isbnNum += sum;
+//     n = Math.floor(n/10);
+// }while(n>0);
+
+// if(isbnNum%11 == 0){
+//     console.log("Valid isbn");    
+// }else console.log("Invalid isbn");
