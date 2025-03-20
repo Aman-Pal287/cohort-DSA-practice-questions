6198d8d6dd63bcbe74573c16b1507c8e65d27364
Aman-Pal287 <palaman9786@gmail.com>
2025-03-20 18:53:49 +0530

    day 34 is done

---
+++ day34/live class/app.js
@@ -0,0 +1,65 @@
+const prompt = require("prompt-sync");
+
+
+// problem :1 mirrored
+//          * 
+//        * * 
+//      * * * 
+//    * * * * 
+//  * * * * * 
+// let n = prompt("enter a number : ");
+// for(let i=1;i<=n;i++){
+//     for(let j = 1 ;j<=n-i;j++){
+//         process.stdout.write("  ")
+//     }
+//     for(let k = 1;k<=i;k++){
+//         process.stdout.write("* ")
+//     }
+//     console.log();
+    
+// }
+
+//
+//       *     
+//      * *    
+//     * * *   
+//    * * * *  
+//   * * * * * 
+
+// let n = prompt("enter a number : ");
+// for(let i=1;i<=n;i++){
+//     for(let j = 1 ;j<=n-i;j++){
+//         process.stdout.write(" ")
+//     }
+//     for(let k = 1;k<=i;k++){
+//         process.stdout.write("* ")
+//     }
+//     console.log();
+// }    
+
+
+// problemm :X pattern
+// let n = Number(prompt("enter a number : "));
+// for(let i= 1;i<=n;i++){
+    //     for(let j=1;j<=n;j++ ){
+        //         if(i==j || i+j === n+1){
+//             process.stdout.write("* ")
+//         }else process.stdout.write("  ")
+//     }
+//     console.log();
+
+// }
+
+
+
+// V pattern
+// let n = Number(prompt("enter a number : "));
+// for(let i= 1;i<=n;i++){
+//     for(let j=1;j<=(n*2)-1;j++ ){
+//         if(i==j || i+j === n*2){
+//             process.stdout.write("* ")
+//         }else process.stdout.write("  ")
+//     }
+//     console.log();
+    
+// }
