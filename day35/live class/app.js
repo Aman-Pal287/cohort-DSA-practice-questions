a12fe5050893462f45a6f6a63d4b75c21c017160
Aman-Pal287 <palaman9786@gmail.com>
2025-03-20 20:02:38 +0530

    day 35 problem 5 is done

--- day35/live class/app.js
+++ day35/live class/app.js
@@ -51,3 +51,14 @@ const prompt = require('prompt-sync')();
 //     }
 // }
 // console.log(max);
+
+
+//problem 5: find the minimum value of an array
+// let arr = [123,15,41,58,2,56,54,56,2,312,54,744,2,65,4,84,874,654,654,654,654,486]
+// let min = arr[0]
+// for(let i= 1;i<arr.length;i++){
+//     if(arr[i]<min){
+//         min = arr[i]
+//     }
+// }
+// console.log(min);
