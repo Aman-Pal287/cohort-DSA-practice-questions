84474a81c8f071a8c87b18a369ee78778b3028e2
Aman-Pal287 <palaman9786@gmail.com>
2025-03-20 19:26:36 +0530

    day 35 problem 1 (strong number)is done

--- day35/live class/app.js
+++ day35/live class/app.js
@@ -1,17 +1,17 @@
 const prompt = require('prompt-sync')();
 // problem 1: strong number
-let n = prompt("Enter a Number")
-let copy = n;
-let sum = 0;
-while(n>0){ 
-    let rem = n%10;
-    let fact = 1
-    for(let i=1 ;i<=rem;i++){
-        fact = fact*i
-    }
-    sum = sum + fact;
-    n = Math.floor(n/10);
-}   
-if(sum == copy) console.log("strong number");
-else console.log("not a strong number");
+// let n = prompt("Enter a Number")
+// let copy = n;
+// let sum = 0;
+// while(n>0){ 
+//     let rem = n%10;
+//     let fact = 1
+//     for(let i=1 ;i<=rem;i++){
+//         fact = fact*i
+//     }
+//     sum = sum + fact;
+//     n = Math.floor(n/10);
+// }   
+// if(sum == copy) console.log("strong number");
+// else console.log("not a strong number");
 
