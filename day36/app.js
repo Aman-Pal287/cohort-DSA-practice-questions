7e205d1415edd5f0698cf2086f180e0ff52f4308
Aman-Pal287 <palaman9786@gmail.com>
2025-03-22 12:21:34 +0530

    day 36 problem 1 done

---
+++ day36/app.js
@@ -0,0 +1,14 @@
+const prompt = require('prompt-sync')();
+// problem 1: second max khojo
+// let arr = [28,28,6,74,2,4,79]
+// let max = Math.max(arr[0] , arr[1]);
+// let sMax = Math.min(arr[0] , arr[1]);
+// for (let i =  2 ; i<arr.length;i++ ){
+//     if(arr[i]>max){
+//         sMax = max
+//         max = arr[i]
+//     }else if(arr[i]>sMax && sMax != max){
+//         sMax = arr[i]
+//     }
+// }
+// console.log("Second max is =  " + sMax);
