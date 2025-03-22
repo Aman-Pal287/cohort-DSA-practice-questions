52e37277e4f528fecab8f3e09534be6ccd75bf8f
Aman-Pal287 <palaman9786@gmail.com>
2025-03-22 12:35:34 +0530

    day 36 home work done

--- day36/app.js
+++ day36/app.js
@@ -7,8 +7,24 @@ const prompt = require('prompt-sync')();
 //     if(arr[i]>max){
 //         sMax = max
 //         max = arr[i]
-//     }else if(arr[i]>sMax && sMax != max){
+//     }else if(arr[i]>sMax && arr[i] != max){
 //         sMax = arr[i]
 //     }
 // }
 // console.log("Second max is =  " + sMax);
+
+
+//problem home work: second minimum number 
+// let arr = [28,28,6,74,2,4,79]
+// let min = Math.min(arr[0] , arr[1])
+// let sMin = Math.max(arr[0] , arr[1])
+// for(let i = 2;i<arr.length;i++){
+//     if(arr[i]< min){
+//         sMin = min;
+//         min = arr[i]
+//     }else if(arr[i]<sMin arr[i] != min){
+//         sMin = arr[i]
+//     }
+// }
+// console.log("second minimum number is = " + sMin);
+
