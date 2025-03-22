eb88b1ba9a619bd242518a9b341bec1ce0b733a4
Aman-Pal287 <palaman9786@gmail.com>
2025-03-22 13:25:45 +0530

    day36 problem 4 is done

--- day36/app.js
+++ day36/app.js
@@ -64,3 +64,14 @@ const prompt = require('prompt-sync')();
 //     i++;
 // }
 // console.log(arr);
+
+
+// problem 4: array left Rotation by 1
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+// let temp = arr[0];
+// for(let i=0; i<arr.length-1;i++){
+//     arr[i] = arr[i+1]
+// }
+// arr[arr.length-1] = temp;
+// console.log(arr);
+
