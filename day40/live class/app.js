2209bdfa1dff96292d9581b42a701d4a9a391140
Aman-Pal287 <palaman9786@gmail.com>
2025-03-28 13:12:36 +0530

    problem 1  method 2 is done

--- day40/live class/app.js
+++ day40/live class/app.js
@@ -14,3 +14,16 @@ const prompt = require('prompt-sync')();
 //     arr[arr.length - 1] = copy
 // }
 // console.log(arr);
+
+//method 2: method 1 ka hi soution hai but k % arr.length karenge taaki unncessary loop naa chale
+// let arr = [1, 2, 3, 4, 5]
+// let k = prompt("enter a number: ")
+// k = k % arr.length;
+// for (let j = 1;j<=k; j++) {
+//     let copy = arr[0]
+//     for (let i = 0; i < arr.length - 1; i++) {
+//         arr[i] = arr[i + 1]
+//     }
+//     arr[arr.length - 1] = copy
+// }
+// console.log(arr);
