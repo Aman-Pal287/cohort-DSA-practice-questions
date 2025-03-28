c20f22cf7ada0cb4df955024a8263ed4c50d45bb
Aman-Pal287 <palaman9786@gmail.com>
2025-03-28 14:50:01 +0530

    problem 1 method 1 is done

--- day40/live class/app.js
+++ day40/live class/app.js
@@ -56,3 +56,18 @@ const prompt = require('prompt-sync')();
 //     }
 // }
 // console.log(arr);
+
+
+//problem 2 : linear search
+// method 1 : brutforce method puree array ke element pe itration karna hoga 
+// let target = Number(prompt("Enter a target value : "));
+// let arr = [10,5,3,15,19,69,20]
+// let index = -1;
+// for(let i =0 ; i<arr.length;i++){
+//     if(arr[i] == target ){
+//         index = i;  
+//         break;
+//     }
+// }
+// if(index == -1) console.log("Element not found");
+// else console.log("Element found at index at " + index+' index');
