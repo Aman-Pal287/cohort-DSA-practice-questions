021bead1224d3373e790577214d085460490ff1a
Aman-Pal287 <palaman9786@gmail.com>
2025-03-29 15:18:55 +0530

    Bubble Sort done

---
+++ day41/live class/app.js
@@ -0,0 +1,19 @@
+let prompt = require('prompt-sync')();
+// sorting question::
+
+
+//what is stable sort and unstable sort - video me samajh lena
+
+//BUBBLE SORT :
+// let arr = [5,3,1,9,7,8,4,3,1,1,4,3,1,8]
+// for(let j= 0;j<arr.length-1;j++){
+//     for(let i = 0 ; i<arr.length-1-j;i++){
+//         if(arr[i]>arr[i+1]){
+//             let temp = arr[i];
+//             arr[i] = arr[i+1];
+//             arr[i+1] = temp;
+//         }    
+//     }
+// }
+// console.log(arr);
+
