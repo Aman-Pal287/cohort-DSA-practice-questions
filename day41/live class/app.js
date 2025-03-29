ad0addd4ce033080c4665f9ebe127234695a918b
Aman-Pal287 <palaman9786@gmail.com>
2025-03-29 15:55:01 +0530

    Selecetion sort done

--- day41/live class/app.js
+++ day41/live class/app.js
@@ -17,3 +17,34 @@ let prompt = require('prompt-sync')();
 // }
 // console.log(arr);
 
+
+//SELECTION SORT
+// let arr = [11, 1, 3, 5, 10, 15]
+
+//mera dimag ka code - ignore maro - ye bc bubble sort jaise hi hogya hai
+// for(let i= 0;i<arr.length-1;i++){
+//     for(let j = i ;j<arr.length-1;j++){
+//         if(arr[j+1]<arr[j]){
+//             let temp = arr[j]
+//             arr[j]=arr[j+1]
+//             arr[j+1] = temp;
+//         }
+//     }
+// }
+// console.log(arr);
+
+//sir ka code
+// for (let i = 0; i < arr.length - 1; i++) {
+//     let small = i;
+//     for (let j = i + 1; j < arr.length; j++) {
+//         if (arr[small] > arr[j]) {
+//             small = j;
+//         }
+//     }
+//     if (i != small) {
+//         let temp = arr[i]
+//         arr[i] = arr[small]
+//         arr[small] = temp
+//     }
+// }
+// console.log(arr);
